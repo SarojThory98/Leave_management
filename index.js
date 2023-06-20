8ad643fac75bf3a974320389919bfe357d3831c1
SarojThory98 <saroj.thory@dianapps.com>
2023-06-21 00:45:16 +0530

    changes in structure

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 const { connectToMongoDb } = require("./src/db");
+const express = require("express");
+const corsMiddleware = require("./src/middlewares/corsMiddleware");
+const app = express();
+
+// Apply the CORS middleware
+app.use(corsMiddleware);
 connectToMongoDb();
